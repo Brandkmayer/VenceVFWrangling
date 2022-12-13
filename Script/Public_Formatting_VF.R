2ee5aa7c47cde61b455e254edd1dc1124c48be3d
Brandon Mayer <brandkmayer@gmail.com>
2022-12-13 15:38:59 -0700

    Added Preprocessing of fence coordinates into excel file with joined metadata. Added script for building shapefiles of shock and sound zones from coordinates and buffering. Added a public script for intended public use as a simple to run script for shapefile processing.

---
+++ Script/Public_Formatting_VF.R
@@ -0,0 +1,11 @@
+# ---
+# title: "Formating Fence GPS points"
+# author: "Brandon Mayer"
+# ---
+
+library(tidyverse)
+library(sf)
+library("rnaturalearth")
+library("rnaturalearthdata")
+library(smoothr)
+
